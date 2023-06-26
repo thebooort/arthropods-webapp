c70777a36fa2ba5fd29bbec0e451505effcd9a2f
Bartolomé Ortiz <18428931+thebooort@users.noreply.github.com>
2023-06-26 20:07:50 +0200

    Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,48 @@
 '''
 
 import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+import wikipedia
+import requests
+import json
+
+WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
+
+def get_wiki_image(search_term):
+    try:
+        result = wikipedia.search(search_term, results = 1)
+        wikipedia.set_lang('en')
+        wkpage = wikipedia.WikipediaPage(title = result[0])
+        title = wkpage.title
+        response  = requests.get(WIKI_REQUEST+title)
+        json_data = json.loads(response.text)
+        img_link = list(json_data['query']['pages'].values())[0]['original']['source']
+        return img_link        
+    except:
+        return 0
+
+wiki_image = get_wiki_image('Cerambyx Cerdo')
+
+
+# Authenticate to Firestore with the JSON account key.
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="arthropods-webapp")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("description").document("qzuXvqwqFOzmsjoqPbRn")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
+
+
+
+
+
