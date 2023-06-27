8772fb5cc7911983a7cc6f1e1d330029a9bd23a8
Bartolomé Ortiz <18428931+thebooort@users.noreply.github.com>
2023-06-27 12:51:00 +0200

    Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,12 +34,12 @@ st.sidebar.markdown('## Selecciona una pestaña')
 
 # Add tabs to the sidebar
 tabs = ["¿Cómo funciona?", "App", "Contacto"]
-selected_tab = st.sidebar.radio("Selecciona una", tabs)
+selected_tab = st.sidebar.radio("", tabs)
 st.sidebar.image("images/ugrlogo.png", use_column_width=True)
 
 if selected_tab == "¿Cómo funciona?":
     # add the logo small
-    image_url = "images/logobicho.png" 
+    image_url = "https://raw.githubusercontent.com/thebooort/arthropods-webapp/main/images/logobicho.png" 
     image_width = 200
     centered_image_html = f"""
 <div style="display: flex; justify-content: center;">
@@ -70,8 +70,8 @@ elif selected_tab == "App":
 
     if user_expertise <5:
         st.write("Tu nivel de conocimientos es bajo. No te preocupes, te ayudaremos con la descripción mostrandote un diagrama para que sepas cómo nombrar diferentes partes comunes de los artrópodos.")
-        st.image("image/esquema de insecto basico.png", use_column_width=True)
-
+        st.image("https://raw.githubusercontent.com/thebooort/arthropods-webapp/main/images/esquema de insecto basico.png" , caption="De: https://leerciencia.net/insectos-las-3-partes-de-su-cuerpo/ Todos los derechos reservados",use_column_width=True)
+        st.markdown("## ¡A describir!")
 
     # get a random row from  df
     df = pd.read_excel('https://raw.githubusercontent.com/thebooort/arthropods-webapp/main/database/animales_filtered.xlsx')
@@ -124,18 +124,6 @@ elif selected_tab == "App":
 
     submit = st.button("¡He terminado!")
 
-    # # Create a reference to the Google post.
-    # doc_ref = db.collection("description").document("qzuXvqwqFOzmsjoqPbRn")
-
-    # # Then get the data at that reference.
-    # doc = doc_ref.get()
-
-    # # Let's see what we got!
-    # st.write("The id is: ", doc.id)
-    # st.write("The contents are: ", doc.to_dict())
-    print(user_description)
-    print(submit)
-    print("aqui")
     if user_description and user_expertise and submit:
         # Authenticate to Firestore with the JSON account key.
         print('¡Enviado info a la base de datos!')
@@ -155,6 +143,10 @@ elif selected_tab == "App":
         'class': random_row['Clase'].to_string(index=False),
         'link_to_photo': photo_url
         })
+
+
+        user_description=""
+        user_description = st.text_input("¡Describe el animal que estás viendo!", max_chars=500,value=user_description)
         st.write("¡Gracias por tu colaboración!")
         st.write("Puedes segui jugando enviando cuantas descripciones quieras :smile:")
         st.write("Aquí te dejamos una descripción generalista del animal que acabas de ver para que puedas aprender algo más sobre él:")
@@ -170,7 +162,47 @@ elif selected_tab == "App":
         st.write(descri)
 
 elif selected_tab == "Contacto":
-    st.write("Welcome to the Contact tab!")
-    # Add your contact information here
+    # Add a title and description for the contact page
+    st.title("Contacto")
+    st.write("Dudas o sugerencias? ¡Escribenos!")
+
+    # Add contact information
+    st.header("Informacion de contacto")
+    st.write("Bartolome Ortiz-Viso")
+    st.write("Centro de investigacion de Tecnologias de la Informacion y Comunicaciones (CITIC) - Universidad de Granada")
+    st.write("Email: bortiz@ugr.es")
+
+    # Add a form for users to enter their contact details and message
+    st.header("Formulario de contacto")
+    name = st.text_input("Nombre")
+    email = st.text_input("Email")
+    message = st.text_area("Mensaje")
+    submit_button = st.button("Enviar")
+
+    # Process the form submission
+    if submit_button:
+        # Validate form inputs
+        if name.strip() == "":
+            st.error("Please enter your name.")
+        elif email.strip() == "":
+            st.error("Please enter your email.")
+        elif message.strip() == "":
+            st.error("Please enter your message.")
+        else:
+            key_dict = json.loads(st.secrets["textkey"])
+            creds = service_account.Credentials.from_service_account_info(key_dict)
+            db = firestore.Client(credentials=creds, project="arthropods-webapp")
+            # generate a random id using uuid
+            id = str(uuid.uuid4())
+            doc_ref = db.collection("mensajes").document(id)
+            doc_ref.set({
+            'email': email,
+            'mensaje': message,
+            'nombre': name
+            })
+            # Process the form data (send email, save to database, etc.)
+            st.success("Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.")
+
+
 
 
